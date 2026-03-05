1b8d321b6fb8a4934e42a6f2b131b2022a26b142
IRENE JOY TRINCHERA <irenejoy15@gmail.com>
2026-03-05 18:35:16 +0800

    Confirmation Code

---
+++ lib/models/confrim_user.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class ConfirmUser {
+  final String email;
+  final String confirmationCode;
+
+  ConfirmUser({required this.email, required this.confirmationCode});
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'email': email,
+      'confirmationCode': confirmationCode,
+    };
+  }
+
+  String toJson() => json.encode(toMap());
+}
