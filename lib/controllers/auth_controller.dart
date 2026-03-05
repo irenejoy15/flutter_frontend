f226a39443a261f231b631f5c4999712fd2bb726
IRENE JOY TRINCHERA <irenejoy15@gmail.com>
2026-03-05 18:52:38 +0800

    Confirm Function Controller

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,9 +1,11 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:shop_app/models/confrim_user.dart';
 import 'package:shop_app/models/user.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop_app/views/confirm_sign_up_screen.dart';
+import 'package:shop_app/views/sign_in_screen.dart';
 class AuthController{
   Future<void> signUpUsers({
       required String email, 
@@ -15,7 +17,7 @@ class AuthController{
         User user = User(email: email, fullName: fullName, password: password);
         http.Response response = await http.post(
           Uri.parse('https://d1b8e92y2m.execute-api.us-east-1.amazonaws.com/sign-up'),
-          headers: {'Content-Type': 'application/json; charset=UTF-8'},
+          headers: <String, String>{'Content-Type': 'application/json; charset=UTF-8'},
           body: user.toJson(),
         );
         if (response.statusCode == 200) {
@@ -35,5 +37,36 @@ class AuthController{
           SnackBar(content: Text('Error signing up user: $e')),
         );
       }
+  }
+
+  Future<void> confirmSignUp({
+    required String email,
+    required String confirmationCode,
+    required BuildContext context
+  }) async {
+    try{
+      ConfirmUser confirmUser = ConfirmUser(email: email, confirmationCode: confirmationCode);
+      http.Response response = await http.post(
+        Uri.parse('https://d1b8e92y2m.execute-api.us-east-1.amazonaws.com/confirm-sign-up'),
+        headers: <String, String>{'Content-Type': 'application/json; charset=UTF-8'},
+        body: confirmUser.toJson(),
+      );
+      if (response.statusCode == 200) {
+        Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context){
+              return SignInScreen();
+            }),
+          );
+      } else {
+        ScaffoldMessenger.of( context).showSnackBar(
+          SnackBar(content: Text(json.decode(response.body)['details'])),
+        );
+      }
+    }catch(e){
+      ScaffoldMessenger.of( context).showSnackBar(
+        SnackBar(content: Text('Error confirming user: $e')),
+      );
+    }
   }
 }
