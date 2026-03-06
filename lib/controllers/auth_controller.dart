35d7efda0b4ca88cea4d67b717e18e56351717c9
IRENE JOY TRINCHERA <irenejoy15@gmail.com>
2026-03-06 15:00:17 +0800

    Sign In Function Controller

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -2,9 +2,11 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:shop_app/models/confrim_user.dart';
+import 'package:shop_app/models/sign_in_user.dart';
 import 'package:shop_app/models/user.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop_app/views/confirm_sign_up_screen.dart';
+import 'package:shop_app/views/main_screens/main_screen.dart';
 import 'package:shop_app/views/sign_in_screen.dart';
 class AuthController{
   Future<void> signUpUsers({
@@ -66,6 +68,37 @@ class AuthController{
     }catch(e){
       ScaffoldMessenger.of( context).showSnackBar(
         SnackBar(content: Text('Error confirming user: $e')),
+      );
+    }
+  }
+
+  Future <void> signInUser({
+    required String email,
+    required String password,
+    required context
+  }) async {
+    try{
+      SignInUser signInUser = SignInUser(email: email, password: password);
+      http.Response response = await http.post(
+        Uri.parse('https://d1b8e92y2m.execute-api.us-east-1.amazonaws.com/sign-in'),
+        headers: <String, String>{'Content-Type': 'application/json; charset=UTF-8'},
+        body: signInUser.toJson(),
+      );
+      if(response.statusCode == 200){
+        Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context){
+              return MainScreen();
+            }),
+          );
+      }else{
+        ScaffoldMessenger.of( context).showSnackBar(
+          SnackBar(content: Text(json.decode(response.body)['details'])),
+        );
+      }
+    }catch(e){
+      ScaffoldMessenger.of( context).showSnackBar(
+        SnackBar(content: Text('Error signing in user: $e')),
       );
     }
   }
