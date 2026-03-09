7fdbd0273721a29ed2a0f75157a788af6d1814c8
IRENE JOY TRINCHERA <irenejoy15@gmail.com>
2026-03-09 17:01:12 +0800

    BannerController

---
+++ lib/controllers/banner_controller.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:shop_app/models/banner_model.dart';
+import 'package:http/http.dart' as http;
+
+class BannerController {
+  Future<List<BannerModel>> fetchBannners() async{
+    http.Response response = await http.get(Uri.parse('https://lkcfa3gfvg.execute-api.us-east-1.amazonaws.com/ads'));
+    if(response.statusCode == 200){
+      final List data = json.decode(response.body);
+      return data.map((e) => BannerModel.fromJson(e)).toList();
+    }else{
+      throw Exception('Failed to load banners');
+    }
+  }
+}
