4499ebe755f8bb3059ecea3cac528956de3fe7cd
IRENE JOY TRINCHERA <irenejoy15@gmail.com>
2026-03-09 16:45:18 +0800

    Search Field

--- lib/views/main_screens/widgets/header_widget.dart
+++ lib/views/main_screens/widgets/header_widget.dart
@@ -25,7 +25,27 @@ class HeaderWidget extends StatelessWidget {
             onTap: (){},
             child: Image.asset('assets/icons/message.png', width: width * 0.08, height: height * 0.08),
           ),
-        ],)
+        ],),
+          SizedBox(height: height * 0.03,),
+          SizedBox(
+            height: height * 0.06,
+            child: TextField(
+              onTap: (){},
+              decoration: InputDecoration(
+                hintText: 'Enter Product Name',
+                hintStyle: TextStyle(fontSize: width * 0.035, color: Color(0xFF7F7F7F)),
+                contentPadding: EdgeInsets.symmetric(vertical: width * 0.03, horizontal: height * 0.015),
+                prefixIcon: Image.asset('assets/icons/searc1.png', width: width * 0.05, height: height * 0.05),
+                suffixIcon: Image.asset('assets/icons/cam.png', width: width * 0.05, height: height * 0.05),
+                fillColor: Colors.grey.shade200,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(width * 0.05),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+            ),
+          ),
       ],),
     );
   }
