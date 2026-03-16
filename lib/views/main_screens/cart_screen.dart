4c3480aed5f633d470995250b66b093445f6b047
IRENE JOY TRINCHERA <irenejoy15@gmail.com>
2026-03-16 12:04:36 +0800

    CART APP BAR

--- lib/views/main_screens/cart_screen.dart
+++ lib/views/main_screens/cart_screen.dart
@@ -1,22 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class CartScreen extends StatefulWidget {
+class CartScreen extends ConsumerStatefulWidget {
   const CartScreen({super.key});
 
   @override
-  State<CartScreen> createState() => _CartScreenState();
+  ConsumerState<CartScreen> createState() => _CartScreenState();
 }
 
-class _CartScreenState extends State<CartScreen> {
+class _CartScreenState extends ConsumerState<CartScreen> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Cart Screen',
-          style: Theme.of(context).textTheme.headlineMedium,
+      appBar: PreferredSize(preferredSize: Size.fromHeight(size.height * 0.18), child: Container(
+        width: size.width,
+        height: size.height * 0.18,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/icons/cartb.png'), 
+            fit: BoxFit.cover
+          )
         ),
-      ),
+        child: Padding(padding: EdgeInsets.symmetric(
+          horizontal: size.width * 0.05,
+          vertical: size.height * 0.02
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('My Cart',style: GoogleFonts.poppins(
+              fontSize: 22, 
+              fontWeight: FontWeight.w600,
+              color: Colors.white
+            ),),
+            Stack(
+              children: [
+                Icon(Icons.shopping_cart, color: Colors.white, size: 28),
+                Positioned(
+                  right: 0,
+                  child: Container(
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Text(
+                      '3', // Example cart item count
+                      style: GoogleFonts.poppins(
+                        fontSize: 12,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+        ),
+      )),
     );
   }
 }
