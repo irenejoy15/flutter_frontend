55d37f743f96e848a27deca423fa32a6ea39471d
IRENE JOY TRINCHERA <irenejoy15@gmail.com>
2026-03-18 13:37:15 +0800

    Product Category Function

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -13,5 +13,20 @@ class ProductController {
       throw Exception('Failed to load products');
     }
   }
+
+  Future<List<ProductModel>> fetchProductsByCategory(String category) async{
+    http.Response response = await http.get(Uri.parse('https://4sfecl9ji9.execute-api.us-east-1.amazonaws.com/products/by-category?category=${Uri.encodeComponent(category)}'));
+    if(response.statusCode == 200){
+      final List data = json.decode(response.body)['products'];
+      return data.map((e) => ProductModel.fromMap(e)).toList();
+    }
+    else if(response.statusCode == 404){
+      return [];
+    }
+    else{
+      throw Exception('Failed to load products by category');
+    }
+  }
+
 }
 // END STEP 2
